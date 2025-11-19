03085156cdb4ddaf03491530858906450b2acaf3
Emi <emiliofuentesoto@hotmail.com>
2025-11-19 18:27:43 +0100

    funcion de tal

--- src/App.java
+++ src/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        funcion();
+    }
+
+    public static void funcion(){
+        System.out.println("Esto es una función");
     }
 }
